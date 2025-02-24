396b08124c436a671bad79e89c2e7f2a4c50f68d
antoh00 <otienoantoh@gmail.com>
2025-02-24 19:24:21 +0300

    Task 3 corrected

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -5,6 +5,6 @@ def common_elements(set_1, set_2):
         if i in set_2:
             result.append(i)
     for i in set_2:
-        if i in set_1:
+        if i in set_1 and i not in result:
             result.append(i)
     return result
